d6fae78838c4ee2f2f0117521b6ac3abf964d469
Thu Trần <thutran@192.168.2.19>
2024-04-04 00:25:57 +0700

    ignore onBrokenLinks config

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: "", // Usually your GitHub org/user name.
   projectName: "", // Usually your repo name.
 
-  onBrokenLinks: "throw",
+  onBrokenLinks: "ignore",
   onBrokenMarkdownLinks: "warn",
 
   // Even if you don't use internationalization, you can use this field to set
