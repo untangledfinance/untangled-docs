1a62aac8b7191e3c8b4273e7e47c82c2f5928645
vi <gmail@tuanm.dev>
2025-10-29 14:45:11 +0700

    fix: update home page redirect link

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,10 +3,10 @@ import { useHistory } from "@docusaurus/router";
 
 export default function Home() {
   const history = useHistory();
-  
+
   useEffect(() => {
     // Redirect to the first page in User Docs
-    history.replace("/docs/OctoVault-Stellar/welcome-to-untangled");
+    history.replace("/docs/Intro/welcome-to-untangled");
   }, [history]);
 
   return null; // Return null since we're redirecting immediately
